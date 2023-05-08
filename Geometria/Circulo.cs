7cd5c9cad8db3c17ddc05344e3dd9c04d0cb3a4c
utnnet2019@outlook.com <utnnet2019@outlook.com>
2023-05-08 17:04:05 -0300

    Agregar archivos de proyecto.

---
+++ Geometria/Circulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Geometria
+{
+    public class Circulo
+    {
+        private int m_radio;
+
+        public int Radio {
+            get {
+                return m_radio;
+            }
+            set {
+                m_radio = value;
+            }
+        }
+
+        public void CalcularPerimetro()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CalcularSuperfie()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
